9706191726f8a488f7e809459da2ae7b13c93466
adf694g <alwyn.burger@uni-due.de>
2019-03-12 13:03:08 +0100

    readded debug

---
+++ lib/debug/debug.h
@@ -0,0 +1,24 @@
+#ifndef _DEBUG_H
+#define _DEBUG_H
+
+#include <main.h>
+
+void debugWriteString(char *str);
+void debugNewLine(void);
+void debugWriteLine(char *str);
+
+void debugWriteStringLength(char *s, uint8_t length);
+void debugWriteChar(uint8_t c);
+void debugWriteHex8(uint8_t num);
+void debugWriteHex16(uint16_t num);
+void debugWriteDec8(uint8_t num);
+void debugWriteDec16(uint16_t num);
+void debugWriteBin16(uint16_t num);
+void debugWriteBin8(uint8_t num);
+void debugWriteHex32(uint32_t num);
+void debugWriteBin32(uint32_t num);
+void debugWriteFloat(float num);
+void debugWriteBin4(uint8_t num);
+void debugWriteDec32(uint32_t num);
+
+#endif
